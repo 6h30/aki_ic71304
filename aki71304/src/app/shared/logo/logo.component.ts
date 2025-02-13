5710fa49d4ce5a5bf898de23a8cf7a1db43d3ea2
6h30 <truongduc1108@gmail.com>
2025-02-13 12:07:09 +0000

    layout update

---
+++ aki71304/src/app/shared/logo/logo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logo',
+  imports: [],
+  templateUrl: './logo.component.html',
+  styleUrl: './logo.component.css'
+})
+export class LogoComponent {
+  mushroomImg: string = '/assets/mushroom.png';
+}
