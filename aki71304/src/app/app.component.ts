2305680e0fadc422fc9e31cf49ce5ff51a3bb273
6h30 <truongduc1108@gmail.com>
2025-02-17 12:15:08 +0000

    layout course

--- aki71304/src/app/app.component.ts
+++ aki71304/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LayoutsComponent } from './layouts/layouts.component';
 import { AccountComponent } from './account/account.component';
+import { CourseComponent } from './course/course.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LayoutsComponent, AccountComponent],
+  imports: [RouterOutlet, LayoutsComponent, AccountComponent, CourseComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ aki71304/src/app/course/course.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course',
+  imports: [],
+  templateUrl: './course.component.html',
+  styleUrl: './course.component.css'
+})
+export class CourseComponent {
+  charizard3d_2Img: string = '/assets/3d_character/charizard4.png';
+  charizard3d_6Img: string = '/assets/3d_character/charizard6.png';
+  dewjemg3dImg: string = '/assets/3d_character/dewjemg.png';
+  articuno3dImg: string = '/assets/3d_character/articuno.png';
+  hooh3dImg: string = '/assets/3d_character/hooh.png';
+  charmeleonImg: string = '/assets/bg/charmeleon.jpg';
+}
